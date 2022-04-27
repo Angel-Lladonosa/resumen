1824c8721459dcfcd2c6004d8a8d2affb954d4f7
joserl <jose.ramos.lucena@iesciudadjardin.com>
2022-04-27 14:50:44 +0200

    add some links

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  url:string;
+  label:string;
+  icon?:string;
+}
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   show: boolean;
+  menu: Array<MenuItem>;
 
   constructor() {
     this.show = false;
+    this.menu = [
+      {
+        label: 'about',
+        url: 'about'
+      },
+      {
+        label: 'education',
+        url: 'education'
+      },
+      {
+        label: 'contact',
+        url: 'contact'
+      },
+      {
+        label: 'skills',
+        url: 'skills'
+      }
+    ];
   }
 
   ngOnInit(): void {
